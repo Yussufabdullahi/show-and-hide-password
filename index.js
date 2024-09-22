2f95998c0142c5b69abf0468c04cc170b780ab8c
= <=>
2024-09-22 08:47:31 +0300

    yuu

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src = "image/eye.png"
+
+    }else{
+        password.type = "password";
+    }
+}
+
